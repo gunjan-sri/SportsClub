3e94d13c9e66fa75783cfb39be79a3fd8cbe1733
gunjan-sri <sgunjan091@gmail.com>
2020-08-29 15:10:28 +0900

    modified app

--- app/routes.py
+++ app/routes.py
@@ -3,10 +3,14 @@ from flask import render_template
 
 @app.route("/")
 
-@app.route("/index")
-def index():
-    return "Hello World!"
-
 @app.route("/home")
 def homepage():
     return render_template('homepage.html', title="Homepage")
+
+@app.route("/sports")
+def sports():
+    return render_template('sports.html', title="Sports Information")
+
+@app.route("/aboutus")
+def aboutus():
+    return render_template('aboutus.html', title="About Us")
